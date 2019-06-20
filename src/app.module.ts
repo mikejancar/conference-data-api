a484422de772776ded8e223c04d9dd2cd742ab9f
Michael Jancar <mikejancar@outlook.com>
2019-06-20 12:32:47 -0400

    Added default route for monitoring purposes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { ConferencesController } from './controllers/conferences/conferences.controller';
 import { SessionsController } from './controllers/sessions/sessions.controller';
 import { ConferenceDataService } from './services/conference-data/conference-data.service';
 
 @Module({
   imports: [],
-  controllers: [ConferencesController, SessionsController],
+  controllers: [AppController, ConferencesController, SessionsController],
   providers: [ConferenceDataService],
 })
 export class AppModule {}
